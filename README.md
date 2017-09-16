d32e74ead2ebcfe2ffa4258f91048115ea6fca97
Dixon Martinez <dixon.22maritnez@gmail.com>
2017-09-15 23:53:41 -0430

    Add source code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# FS_Venezuela
+# README #
+
+# FacturaScripts
+*Software de facturación y contabilidad para pymes, fácil, libre y con actualizaciones constantes. Es compatible con FacturaLux, Abanq y Eneboo. Es software libre bajo licencia GNU/LGPL.*
+
+## Instalación
+Para instalarlo sólo necesitas PHP 5.3 o superior, PostgreSQL o MySQL. Puedes ver documentación más detallada en la documentación:
+https://www.facturascripts.com/documentacion
+
+## Feedback
+Para cualquier duda, problema o sugerencia:
+https://www.facturascripts.com/feedback
+
+## Colaborar
+Si desdeas colaborar puedes dirigirte a la sección en nuestra web con todas las tareas pendientes, documentación y chat para programadores:
+https://www.facturascripts.com/tareas
+
+## FS_Venezuela
+FS_Venezuela es el plugin desarrollado implementar FacturaScripts sin problemas en Venezuela.
+
+Este plugin está basado en las instrucciones encontradas en tutoriales de Factura Scripts, con el podrás configurar con un clic 
+1.- El país Venezuela configurado como "País por defecto"
+2.- La moneda VEF configurada como "Moneda por defecto"
+3.- Establecer impuestos de IVA aplicados en Venezuela
+4.- Importar datos contables.
---
+++ controller/admin_fs_venezuela.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * This file is part of FacturaSctipts
+ * Copyright (C) 2015-2017  Carlos Garcia Gomez  neorazorx@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+require_model('impuesto.php');
+require_model('ejercicio.php');
+require_model('subcuenta.php');
+
+/**
+ * Admin FS_Venezuela
+ *
+ * @author Dixon Martinez
+ */
+class admin_fs_venezuela extends fs_controller {
+
+    public function __construct() {
+        parent::__construct(__CLASS__, 'FS Venezuela', 'admin');
+    }
+
+    private $taxes = array(
+            "iva12" => array(
+                "value" => "IVA12",
+                "desc" => "IVA 12%",
+                "porcRecar" => 0,
+                "porc" => 12
+            ), "exc" => array(
+                "value" => "EXENTO",
+                "desc" => "EXENTO",
+                "porcRecar" => 0,
+                "porc" => 0
+            ), "iva8" => array(
+                "value" => "IVA8",
+                "desc" => "IVA 8%",
+                "porcRecar" => 0,
+                "porc" => 8
+            ),
+        );
+    
+    protected function private_core() {
+        if (filter_input(INPUT_GET, "opc")) {
+            $opc = filter_input(INPUT_GET, "opc");
+            $value = filter_input(INPUT_GET, "value");
+            if (!empty($value)) {
+                if ($opc == "country" && $value != $this->empresa->codpais) {
+                    $this->empresa->codpais = $value;
+                    $msg = $this->empresa->save() ? "Empresa actualizada correctamente" : "Ya es el país por defecto ó no seleccionastes algún país";
+                    $this->new_message($msg);
+                } else if ($opc == "currency" && $value != $this->empresa->coddivisa) {
+                    $this->empresa->coddivisa = $value;
+                    $msg = $this->empresa->save() ? "Empresa acutalizada correctamente" : "Ya es la moneda por defecto ó no seleccionastes alguna moneda";
+                    $this->new_message($msg);
+                }
+            }
+            if ($opc == "taxes") {
+                $this->new_message("Configurando Impuestos");
+                $this->set_taxes();
+                $this->new_message("Impuestos agregados correctamente");
+            } else if($opc == "regimen") {
+                $this->new_message("Estableciendo Regímenes de Impuesto");
+                /// Si hay usa lista personalizada en fs_vars, la usamos
+                $fsvar = new \fs_var();
+                $fsvar->simple_save('cliente::regimenes_iva', 'IVA, EXCENTO') ? $this->new_message('Datos guardados correctamente.') : $this->new_message('Los Datos no fueron guardados.');
+            }
+        } else {
+            $this->check_accounting_year();$this->share_extensions();
+        }
+    }
+
+    /**
+     * Export/Import Documents Account
+     * Exportar/Importar Documentos de Cuenta Contable
+     */
+    private function share_extensions() {
+        $fsext = new fs_extension();
+        $fsext->name = 'puc_venezuela';
+        $fsext->from = __CLASS__;
+        $fsext->to = 'contabilidad_ejercicio';
+        $fsext->type = 'fuente';
+        $fsext->text = 'PUC Venezuela';
+        $fsext->params = 'plugins/FS_Venezuela/extras/venezuela.xml';
+        $fsext->save();
+    }
+    /**
+     * Verify is Accounting Year
+     */
+    private function check_accounting_year() {
+        $sc = new ejercicio();
+        foreach ($sc->all_abiertos() as $accountingYear) {
+            if ($accountingYear->longsubcuenta != 6) {
+                $accountingYear->longsubcuenta = 6;
+                $accountingYear->save() ?
+                        $this->new_message('Datos del ejercicio ' . $accountingYear->codejercicio . ' modificados correctamente.') 
+                        : $this->new_error_msg('Error al modificar el ejercicio.');
+            }
+        }
+    }
+
+    /**
+     * Set Taxes
+     * Set taxes by Venezuelan
+     */
+    private function set_taxes() {
+        $allTaxes = new impuesto(); //  Delete taxes existing
+        foreach ($allTaxes->all() as $tax) {
+            $tax->delete();
+        }
+        foreach ($this->taxes as $key => $value) {
+            $tax = new impuesto();
+            $tax->codimpuesto = $value["value"];
+            $tax->descripcion = $value["desc"];
+            $tax->recargo = $value["porcRecar"];
+            $tax->iva = $value["porc"];
+            $tax->save();
+        }
+    }
+
+    /**
+     * Verify if Currency is Aligne Right
+     * @return boolean
+     */
+    public function is_format_currency() {
+        if (FS_POS_DIVISA == 'right') {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Verify if exists Tax of Venezuelan
+     * @return boolean
+     */
+    public function is_set_tax() {
+        $opc = false;        
+        foreach ($this->taxes as $key => $value) {
+            $tax = new impuesto(); //   Instancio al impuesto
+            $tax->codimpuesto = $value['value']; // Inicializo el codigo del impuesto NOTA: Averiguar una mejor forma de consultar a la base de datos
+            $opc = $tax->exists(); //   Verifico si existe el impuesto y lo asigno a  la variable
+            //  Nota mejorar método para consultar si existen
+        }        
+        return $opc;
+    }
+    /**
+     * Estable el plan de cuentas
+     * @return boolean
+     */
+    public function is_set_accounting_year() {
+        $accountingYear0 = new ejercicio();
+        $accountingYear = $accountingYear0->get_by_fecha($this->today());
+        if ($accountingYear) {
+            $subAccountingYear0 = new subcuenta();
+            foreach ($subAccountingYear0->all_from_ejercicio($accountingYear->codejercicio) as $sc) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
