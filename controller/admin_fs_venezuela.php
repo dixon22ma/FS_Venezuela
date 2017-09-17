07d39cbb246037fe556fffca175d7287d2f1632a
Dixon Martinez <dixon.22maritnez@gmail.com>
2017-09-16 22:00:43 -0430

    Refactoring code

--- controller/admin_fs_venezuela.php
+++ controller/admin_fs_venezuela.php
@@ -33,48 +33,54 @@ class admin_fs_venezuela extends fs_controller {
     }
 
     private $taxes = array(
-            "iva12" => array(
-                "value" => "IVA12",
-                "desc" => "IVA 12%",
-                "porcRecar" => 0,
-                "porc" => 12
-            ), "exc" => array(
-                "value" => "EXENTO",
-                "desc" => "EXENTO",
-                "porcRecar" => 0,
-                "porc" => 0
-            ), "iva8" => array(
-                "value" => "IVA8",
-                "desc" => "IVA 8%",
-                "porcRecar" => 0,
-                "porc" => 8
-            ),
-        );
-    
+        "iva12" => array(
+            "value" => "IVA12",
+            "desc" => "IVA 12%",
+            "porcRecar" => 0,
+            "porc" => 12
+        ), "exc" => array(
+            "value" => "EXENTO",
+            "desc" => "EXENTO",
+            "porcRecar" => 0,
+            "porc" => 0
+        ), "iva8" => array(
+            "value" => "IVA8",
+            "desc" => "IVA 8%",
+            "porcRecar" => 0,
+            "porc" => 8
+        ),
+    );
+
     protected function private_core() {
         $value = filter_input(INPUT_GET, "value");
-        if(!empty(filter_input(INPUT_GET, "opc")) ) {
+        if (!empty(filter_input(INPUT_GET, "opc"))) {
             switch (filter_input(INPUT_GET, "opc")) {
-                case "country" && !empty($value): $this->set_country_code($value); break;
-                case "currency" && !empty($value): $this->set_currency($value); break;
-                case "taxes": $this->set_taxes(); break;
-                case "regimen": $this->set_regimen(); break;
+                case "country" && !empty($value): $this->set_country_code($value);
+                    break;
+                case "currency" && !empty($value): $this->set_currency($value);
+                    break;
+                case "taxes": $this->set_taxes();
+                    break;
+                case "regimen": $this->set_regimen();
+                    break;
             }
-            $this->check_accounting_year(); $this->share_extensions();
+            $this->check_accounting_year();
+            $this->share_extensions();
         }
     }
-    
+
     private function set_regimen() {
         $this->new_message("Estableciendo Regímenes de Impuesto");
         $fsvar = new \fs_var(); // Si hay una lista personalizada en fs_vars, la usamos
         $fsvar->simple_save('cliente::regimenes_iva', 'IVA, EXCENTO, IVA8') ? $this->new_message('Datos guardados correctamente.') : $this->new_message('Los Datos no fueron guardados.');
     }
+
     private function set_country_code($value) {
         $this->empresa->codpais = $value;
         $msg = $this->empresa->save() ? "Empresa actualizada correctamente" : "Ya es el país por defecto ó no seleccionastes algún país";
-        $this->new_message($msg);                    
+        $this->new_message($msg);
     }
-    
+
     private function set_currency($value) {
         $this->empresa->coddivisa = $value;
         $msg = $this->empresa->save() ? "Empresa acutalizada correctamente" : "Ya es la moneda por defecto ó no seleccionastes alguna moneda";
@@ -95,6 +101,7 @@ class admin_fs_venezuela extends fs_controller {
         $fsext->params = 'plugins/FS_Venezuela/extras/venezuela.xml';
         $fsext->save();
     }
+
     /**
      * Verify is Accounting Year
      */
@@ -104,8 +111,7 @@ class admin_fs_venezuela extends fs_controller {
             if ($accountingYear->longsubcuenta != 6) {
                 $accountingYear->longsubcuenta = 6;
                 $accountingYear->save() ?
-                        $this->new_message('Datos del ejercicio ' . $accountingYear->codejercicio . ' modificados correctamente.') 
-                        : $this->new_error_msg('Error al modificar el ejercicio.');
+                                $this->new_message('Datos del ejercicio ' . $accountingYear->codejercicio . ' modificados correctamente.') : $this->new_error_msg('Error al modificar el ejercicio.');
             }
         }
     }
@@ -147,20 +153,22 @@ class admin_fs_venezuela extends fs_controller {
      * @return boolean
      */
     public function is_set_tax() {
-        $opc = false;        
+        $opc = false;
         foreach ($this->taxes as $value) {
             $tax = new impuesto(); //   Instancio al impuesto
             $tax->codimpuesto = $value['value']; // Inicializo el codigo del impuesto NOTA: Averiguar una mejor forma de consultar a la base de datos
             $opc = $tax->exists(); //   Verifico si existe el impuesto y lo asigno a  la variable
             //  Nota mejorar método para consultar si existen
-        }        
+        }
         return $opc;
     }
-    
+
     public function is_set_regimenes() {
         $fsvar = new \fs_var(); // Si hay una lista personalizada en fs_vars, la usamos
-        if(!$fsvar->simple_get("cliente::regimenes_iva")) { return false; } 
-        return true;
+        if (!$fsvar->simple_get("cliente::regimenes_iva")) {
+            return FALSE;
+        }
+        return TRUE;
     }
 
     /**
@@ -173,10 +181,11 @@ class admin_fs_venezuela extends fs_controller {
         if ($accountingYear) {
             $subAccountingYear0 = new subcuenta();
             foreach ($subAccountingYear0->all_from_ejercicio($accountingYear->codejercicio) as $sc) {
-                return true;
+                echo $sc;
+                return TRUE;
             }
         }
-        return false;
+        return FALSE;
     }
 
 }
